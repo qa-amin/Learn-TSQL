e3f8968aeeb86d39ff3e6b7171981ec9c9011b3c
qa-amin <m.a.ghaderi78@gmail.com>
2022-03-04 18:50:23 +0330

    add ORDER BY

---
+++ 03-SELECT.sql
@@ -0,0 +1,26 @@
+﻿use MyDB;
+GO
+
+-- SELECT بدون جدول
+SELECT
+	N'امین';
+GO
+-- Order انتخاب تمامی ستون های جدول
+
+SELECT 
+	*
+FROM dbo.Orders;
+GO
+/*
+	Alias
+	<expression> AS <alias>   
+	<expression> <alias>
+	<alias> = <expression>
+*/
+
+-- <expression> AS <alias>
+
+SELECT 
+	EmployeeID, YEAR(OrderDate) AS OrderYear
+FROM dbo.Orders;
+
---
+++ 04-ORDER BY.sql
@@ -0,0 +1,32 @@
+﻿USE MyDB;
+GO
+
+-- EmployeID مرتب سازی بر روی ستون 
+SELECT 
+	EmployeeID, YEAR(OrderDate) AS OrderYear
+FROM dbo.Orders
+	WHERE CustomerID = 71
+ORDER BY EmployeeID ;   -- ASC DESC
+GO
+
+SELECT
+	EmployeeID, YEAR(OrderDate) AS OrderYear
+FROM dbo.Orders
+	WHERE CustomerID = 71
+ORDER BY OrderYear DESC;
+GO
+
+-- مرتب سازی بر روی دو ستون
+
+SELECT 
+	EmployeeID, Region, City
+FROM dbo.Employees
+ORDER BY Region, City;
+GO
+
+-- شرکت داده نشده اندSELECT مرتب سازی بروی ستونی که در  
+SELECT
+	EmployeeID, FirstName, LastName
+FROM dbo.Employees
+ORDER BY City;
+GO

01a3cecdfc230b76ea980a7e3520a158f1b1d7d6
qa-amin <m.a.ghaderi78@gmail.com>
2022-03-06 23:19:26 +0330

    add 3VL

---
+++ 08-3VL.sql
@@ -0,0 +1,45 @@
+﻿USE MyDB;
+GO
+
+-- Accept TRUE
+SELECT
+	CustomerID, State, Region, City
+FROM dbo.Customers
+	WHERE Region = N'جنوب';
+GO
+
+SELECT  -- 91 record - 1 record = 90 record  but	output is 31 !!!!!!!!
+	CustomerID, State, Region, City
+FROM dbo.Customers
+	WHERE Region <> N'جنوب';
+GO
+-- NULL آن ها برابر است با  Region فهرست مشتریانی که مقدار 
+SELECT
+	CustomerID, State, Region, City
+FROM dbo.Customers
+	WHERE  Region IS NULL;
+GO
+
+-- آن ها صرفا جنوب نیست Region فهرست مشتریانی که مقدار فیلد
+
+SELECT
+	CustomerID, State, Region, City
+FROM dbo.Customers
+	WHERE Region <> N'جنوب'
+	OR
+	Region IS NULL;
+GO
+
+/*
+ISNULL تابع
+بایک مقدار مشخص NULL جایگزین کردن مقدار 
+
+ISNULL(var,'every thing')
+*/
+
+-- آن ها صرفا جنوب نیست Region فهرست مشتریانی که مقدار فیلد
+SELECT 
+	CustomerID, State, Region, City
+FROM dbo.Customers
+	WHERE ISNULL(Region,'') <> N'جنوب';
+GO
